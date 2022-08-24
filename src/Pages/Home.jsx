da2b5463b07bec1ba83d3f4bc15ff8986fdbbc07
EricFooSD <fooshida@gmail.com>
2022-08-24 18:58:18 +0800

    added comments for homepage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
+// =================  IMPORT =========================>
 import * as React from 'react';
 import ProductCategories from '../components/Homepage/ProductCategories.jsx';
 import ProductSmokingHero from '../components/Homepage/ProductSmokingHero.jsx';
 import ProductHero from '../components/Homepage/ProductHero.jsx';
 import ProductValues from '../components/Homepage/ProductValues.jsx';
 import ProductHowItWorks from '../components/Homepage/ProductHowItWorks.jsx';
+// ===================================================>
 
 export default function Home() {
   return (
