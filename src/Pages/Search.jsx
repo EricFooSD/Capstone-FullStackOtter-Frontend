df0d71db3373172a70864de6454201b8457b719a
EricFooSD <fooshida@gmail.com>
2022-08-24 19:12:19 +0800

    added comments for search page

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -1,27 +1,17 @@
 /* eslint-disable max-len */
+
+// =================  IMPORT =========================>
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {
-  Grid,
-  Box,
-  Container,
-  Table,
-  TableRow,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TablePagination,
-  Paper,
-  Avatar,
-  Typography,
+  Grid, Box, Container, Table, TableRow, TableBody, TableCell, TableContainer, TableHead, TablePagination, Paper, Avatar, Typography,
 } from '@mui/material';
 import SearchBar from '../components/SearchPage/SearchBar.jsx';
 import BACKEND_URL from '../supportFunctions.js';
 import ProfileModalForSearch from '../components/SearchPage/ProfileModalForSearch.jsx';
+// ===================================================>
 
-// ----------------------------------------------------------------------
-
+// .......... TABLE HEADERS ............ //
 const TABLE_HEAD = [
   { id: 'name', label: 'Name', minWidth: 170 },
   { id: 'location', label: 'Location', minWidth: 100 },
@@ -33,13 +23,14 @@ const TABLE_HEAD = [
   },
 ];
 
-// ----------------------------------------------------------------------
-
 export default function Search() {
+  // .......... STATES ............ //
   const [searchQuery, setSearchQuery] = useState('');
   const [users, setUsers] = useState([]);
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  // .......... HELPER FUNCTIONS ............ //
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -70,15 +61,15 @@ export default function Search() {
     getAllUsers();
   }, []);
 
+  // filtering data for search
   const filterData = (query, data) => {
-    // if (!query) {
-    //   return null;
-    // }
     const searchResults = data.filter((user) => user.skills.find((skill) => skill.name.toLowerCase().includes(query)) || user.name.toLowerCase().includes(query));
     return searchResults;
   };
 
   const dataFiltered = filterData(searchQuery, users);
+
+  // .......... RENDERING COMPONENT .......... //
 
   return (
     <>
